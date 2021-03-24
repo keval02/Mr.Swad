befbc8be5bd03f9af4af6d4892b5b2be8dc86f26
keval <kevals02@outlook.com>
2021-03-24 17:27:22 +0530

    Initial Commit.

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Android\Fluttersdk"
+export "FLUTTER_APPLICATION_PATH=D:\New folder\codecanyon-5Y6G1dbZ-grocery-food-pharmacy-store-delivery-mobile-app-with-admin-panel\flutter_application"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=D:\Android\Fluttersdk\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.7.0"
+export "FLUTTER_BUILD_NUMBER=1.7.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
